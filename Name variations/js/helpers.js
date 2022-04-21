13d5118387ca1eb87753d7037cc13abae1ea4ada
Ekaterina <lyubavskayaea@inbox.ru>
2022-04-21 16:47:17 +0300

    Name variation ошибка json

---
+++ Name variations/js/helpers.js
@@ -0,0 +1,16 @@
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    return name.length
+  };
+  
+  
+  /** @param {string} name */
+  export function getLower(name) {
+    return name.toLowerCase
+  };
+  
+  /** @param {string} name */
+  export function getUpper(name) {
+    return name.toUpperCase
+  };
+
