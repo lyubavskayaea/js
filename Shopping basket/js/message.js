90cb56f02f711077afed9e246e8b3b5be52c904b
Ekaterina <lyubavskayaea@inbox.ru>
2022-04-26 09:02:50 +0300

    Shopping basket

---
+++ Shopping basket/js/message.js
@@ -0,0 +1,13 @@
+/* message.js */
+export function getMessage(value) {
+    console.log(value)
+      if (value < 0){
+        return "Invalid number"
+      } else if (value === 0){
+        return "You don't have any items in your shopping list"
+      } else if (value === 1) {
+        return "You have one item in your shopping list"
+      } else if (value > 1) {
+        return "You have more than 1 item in your shopping list"
+      }
+  }
