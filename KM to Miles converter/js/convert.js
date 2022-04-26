4d965cf3b56358a2c0ad5fed778eb8282ce5cfe0
Ekaterina <lyubavskayaea@inbox.ru>
2022-04-26 17:14:34 +0300

    KM to Miles converter

---
+++ KM to Miles converter/js/convert.js
@@ -0,0 +1,5 @@
+/* convert.js */
+export function convertKMtoMiles(km) {
+	return km * 0.62137
+	// your code
+}
---
+++ KM to Miles converter/js/index.js
@@ -0,0 +1,9 @@
+/* index.js */
+import {convertKMtoMiles} from "./convert.js"
+
+const kmInput = document.querySelector("#km-input")
+const milesOutput = document.querySelector("#miles-output")
+
+kmInput.addEventListener("keyup", () => {
+  milesOutput.value = convertKMtoMiles(kmInput.value)
+})
