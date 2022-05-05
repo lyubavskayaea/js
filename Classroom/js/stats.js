a2a17658ba9131080f9a8a3f85177269312a3b82
Ekaterina <lyubavskayaea@inbox.ru>
2022-05-05 10:53:23 +0300

    Подсчет оценок

---
+++ Classroom/js/stats.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * @param {array} grades
+ */
+ export function getNumberOfGrades(grades) {
+	return grades.length// your code
+}
+
+/**
+ * @param {array} grades
+ */
+export function getFirstGrade(grades) {
+	return grades[0]// your code
+}
+
+/**
+ * @param {array} grades
+ */
+export function getLastGrade(grades) {
+	return grades[grades.length - 1]
+	// your code
+}
