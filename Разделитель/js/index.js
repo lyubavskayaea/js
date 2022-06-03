72513cc07ea4ac1a9a395815efc0a6a8a937b40c
Ekaterina <lyubavskayaea@inbox.ru>
2022-06-03 12:40:29 +0300

    homework

---
+++ Разделитель/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} name
+ */
+ const getSlug = name => {
+	 return name.replaceAll(" ", "-").toLowerCase();
+	// your code
+}
+
+// Sample usage - do not modify
+console.log(getSlug("IKEA table")); // "ikea-table"
+console.log(getSlug("200cm Bed")); // "200cm-bed"
+console.log(getSlug("Bedside lavalamp")); // "bedside-lavalamp"
+console.log(getSlug("A B C noodles")); // "a-b-c-noodles"
