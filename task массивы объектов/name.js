46dcac85d0fd386e4a21d8284e1077306abe2057
Ekaterina <lyubavskayaea@inbox.ru>
2022-06-11 11:46:09 +0300

    task

--- task массивы объектов/name.js
+++ task массивы объектов/name.js
@@ -5,8 +5,12 @@
  * @param {string} users.lastName
  */
  const logNames = users => {
-    return `logs: "${users[0].firstName} ${users[0].lastName}" and "${users[1].firstName} ${users[1].lastName}"`
-  
+    //return `logs: "${users[0].firstName} ${users[0].lastName}" and "${users[1].firstName} ${users[1].lastName}"`
+    return users.map(function (user) {
+
+      return `${user.firstName} ${user.lastName}`
+
+  }).join(" and ")
 }
 
 // Sample usage - do not modify
