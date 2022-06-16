dfab8ab001eb3181f96b96c8e5e86e4e039ee966
Ekaterina <lyubavskayaea@inbox.ru>
2022-06-16 15:57:13 +0300

    class

---
+++ task классы/mistake.js
@@ -0,0 +1,22 @@
+// class definition
+class Course {
+    constructor(name, isCompleted) {
+      this.name = name;
+      this.isCompleted = isCompleted;
+    }
+  
+    getDescription() {
+      if (this.isCompleted) {
+        return `You have completed the ${this.name} course.`;
+      } else {
+        return `You are currently studying the ${this.name} course.`;
+      }
+    }
+  }
+  
+  // Sample usage - do not modify
+  const course1 = new Course("Learn JS", false);
+  console.log(course1.getDescription()); // "You are currently studying the Learn JS course"
+  
+  const course2 = new Course("Learn React", true);
+  console.log(course2.getDescription()); // "You have completed the Learn React course"
